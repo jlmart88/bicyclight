745e5edbf059bd18c35434fb0f4290ed74837e3e
Nalini Singh <nmsingh@mit.edu>
2016-02-20 15:26:37 -0500

    Read accelerometer readings from wristband

--- prototype2/backpack/backpack.ino
+++ prototype2/backpack/backpack.ino
@@ -1,3 +1,10 @@
+// RF setup
+#include <SPI.h>
+#include "RF24.h"
+
+bool radioNumber = 0;
+RF24 radio(9,10);
+byte addresses[][6] = {"1Node","2Node"};
 
 // EL switches
 #define LEFT_LIGHT 3
@@ -37,14 +44,74 @@ void setup() {
   pinMode(LEFT_LIGHT, OUTPUT);  // channel A  
   pinMode(RIGHT_LIGHT, OUTPUT);  // channel B   
   pinMode(CENTER_LIGHT, OUTPUT);  // channel C
+
+  Serial.begin(38400);
   
+  // rf
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
+  }else{
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1,addresses[1]);
+  }
   
+  radio.startListening();  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   _lightStatusUpdate();
-}
+
+  // get YPR readings
+
+  radio.startListening();
+
+    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
+    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
+    
+    while ( ! radio.available() ){                             // While nothing is received
+      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
+          timeout = true;
+          break;
+      }      
+    }
+        
+    if ( timeout ){                                             // Describe the results
+        Serial.println(F("Failed, response timed out."));
+    }else{
+        char ypr[sizeof(float)*3];                                 // Get the response       
+        byte yaw_bytes[sizeof(float)];
+        byte pitch_bytes[sizeof(float)];
+        byte roll_bytes[sizeof(float)];
+        
+        radio.read( &ypr, sizeof(float)*3 );
+        unsigned long time = micros();
+
+        memcpy(yaw_bytes,ypr,sizeof(float));
+        float yaw = *((float *) yaw_bytes);
+
+        memcpy(pitch_bytes,&(ypr[sizeof(float)]),sizeof(float));
+        float pitch = *((float *) pitch_bytes);
+        
+        memcpy(roll_bytes,&(ypr[sizeof(float)*2]),sizeof(float));
+        float roll = *((float *) roll_bytes);
+        
+        Serial.print("yaw: ");
+        Serial.print(yaw);
+        Serial.print(" pitch: ");
+        Serial.print(pitch);
+        Serial.print(" roll: ");
+        Serial.println(roll);       
+    }
+
+    // Try again 1s later
+    delay(1000);
+  }
 
 // call frequently throughout the loop() to simulate asynchronous light updating
 void _lightStatusUpdate() {
