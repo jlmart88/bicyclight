17a337a05c4cdcccb133bc2a483519586ad63209
Nalini Singh <nmsingh@mit.edu>
2016-02-20 12:00:34 -0500

    Add wristband files and hardware definitions

---
+++ prototype2/wristband/wristband.ino
@@ -0,0 +1,22 @@
+// Accelerometer
+#define INT 2
+#define SCL A4
+#define SDA A5
+
+// Vibe
+#define VIBE D5
+
+// LEDs
+#define LED1 A0
+#define LED2 A1
+
+void setup() {
+  
+  
+}
+
+void loop() {
+
+
+}
+
