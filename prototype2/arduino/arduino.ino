7b0ea68979cb60eaf263a8482eaa066fbca92257
Nalini Singh <nmsingh@mit.edu>
2016-02-20 11:11:36 -0500

    Initial prototype 2 commit

---
+++ prototype2/arduino/arduino.ino
@@ -0,0 +1,22 @@
+
+// EL switches
+#define LEFT_LIGHT D3
+#define RIGHT_LIGHT D4
+#define CENTER_LIGHT D5
+
+// Accelerometer
+
+
+// RF Module
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
