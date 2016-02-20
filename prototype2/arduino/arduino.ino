00c8b879ca82d767112042a14d06135eb97a22d8
Nalini Singh <nmsingh@mit.edu>
2016-02-20 11:50:01 -0500

    Hardware definitions

--- prototype2/arduino/arduino.ino
+++ prototype2/arduino/arduino.ino
@@ -1,15 +1,17 @@
 
 // EL switches
-#define LEFT_LIGHT D3
-#define RIGHT_LIGHT D4
-#define CENTER_LIGHT D5
+#define LEFT_LIGHT 3
+#define RIGHT_LIGHT 4
+#define CENTER_LIGHT 5
 
 // Accelerometer
+#define INT 2
+#define SCL A4
+#define SDA A5
 
-
-// RF Module
-
-
+// LEDs
+#define LED1 A0
+#define LED2 A1
 
 void setup() {
   // put your setup code here, to run once:
